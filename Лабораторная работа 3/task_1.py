5d5afea93353b2a8b230e6bd14ceddcacdda795c
Asriel_Story <73496818+AsrielStory@users.noreply.github.com>
2025-01-28 16:07:55 +0300

    Лабораторная работа 3
    Первая попытка отправки лабораторной работы 3

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,33 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if type(pages) != int or pages <= 0:
+            raise ValueError
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if type(duration) != float or duration <= 0:
+            raise ValueError
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration})"
